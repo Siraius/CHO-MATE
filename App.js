18b4b3c6c1b0c1c4e4db7e72edd10cd4b4b6821f
Mortimore316 <kclittlered12@gmail.com>
2022-10-05 01:33:29 -0500

    Background

--- App.js
+++ App.js
@@ -1,7 +1,30 @@
 import { Provider, DefaultTheme } from 'react-native-paper';
-import { Text } from 'react-native';
+import { useEffect } from 'react';
 import colors from './app/config/colors';
 import RootNavigator from './app/navigation/RootNavigator';
+
+import * as BackgroundFetch from 'expo-background-fetch';
+import * as TaskManager from 'expo-task-manager';
+
+
+const BACKGROUND_FETCH_TASK = 'background-fetch';
+
+TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
+  const now = Date.now();
+
+  console.log(`Got background fetch call at date: ${new Date(now).toISOString()}`);
+
+  // Be sure to return the successful result type!
+  return BackgroundFetch.BackgroundFetchResult.NewData;
+});
+
+async function registerBackgroundFetchAsync() {
+  return BackgroundFetch.registerTaskAsync(BACKGROUND_FETCH_TASK, {
+    minimumInterval: 60 * 15, // 15 minutes
+    stopOnTerminate: false, // android only,
+    startOnBoot: true, // android only
+  });
+}
 
 const theme = {
   ...DefaultTheme,
@@ -14,6 +37,18 @@ const theme = {
 };
 
 export default function App() {
+
+  useEffect(() => {
+    checkStatusAsync();
+  }, []);
+
+  const checkStatusAsync = async () => {
+    registerBackgroundFetchAsync();
+    const status = await BackgroundFetch.getStatusAsync();
+    const isRegistered = await TaskManager.isTaskRegisteredAsync(BACKGROUND_FETCH_TASK);
+    console.log("Status: ", status);
+    console.log("isRegistered: ", isRegistered);
+  };
   return (
     <Provider theme={theme}>
       <RootNavigator />
